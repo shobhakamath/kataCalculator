3a878156b6f8a34f239c31f100adb2f230a6e2b8
shobhakamath <shobha.s.kamath@gmail.com>
2020-07-05 18:48:06 +0400

    negative not allowed exception added

--- src/main/java/com/kata/Calculator.java
+++ src/main/java/com/kata/Calculator.java
@@ -1,6 +1,11 @@
 package com.kata;
 
-import java.text.MessageFormat;
+import com.kata.exception.CalculatorException;
+import com.kata.util.CalculatorUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Calculator {
     static final String SEPARATOR = ",|\n";
@@ -10,17 +15,28 @@ public class Calculator {
      * @param numbers : numbers is the string being passed
      * @return int : returns an int which is the addition of utmost 2 int values
      */
-    public static int Add(String numbers) {
+    public static int Add(String numbers) throws CalculatorException{
         float sum = 0;
         if (numbers != null && numbers.length() > 0) {
             String newDelimiter = CalculatorUtil.getNewDelimiter(numbers);
             String[] split = CalculatorUtil.getFinalString(numbers, newDelimiter).split(SEPARATOR +
                     (newDelimiter != null && newDelimiter.length() > 0 ? "|" + newDelimiter : ""));
+
+            List<Float> lstFloat=new ArrayList<>();
             //The method can take any number of numbers and count them
             for (String str : split) {
                 if (CalculatorUtil.isNumeric(str)) {
-                    sum += Float.parseFloat(str);
+                    float floatVal=Float.parseFloat(str);
+                    if(floatVal<0){
+                        lstFloat.add(floatVal);
+                    }
+                    if(lstFloat.size()==0)
+                         sum +=floatVal;
+
                 }
+            }
+            if(lstFloat.size()>0){
+                throw new CalculatorException(String.valueOf(lstFloat));
             }
 
         }
