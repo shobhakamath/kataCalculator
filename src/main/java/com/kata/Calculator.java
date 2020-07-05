5c44863812d0bd4ac638d9bfea05b6c763b32284
shobhakamath <shobha.s.kamath@gmail.com>
2020-07-05 15:56:04 +0400

    Added more test cases.Renamed the test class There is no mention of requiring to scale up the values of decimal values.So it will be treated as an integer. Accomodated these changes as well. Code refactored.Fixed sonarqube issues.

--- src/main/java/com/kata/Calculator.java
+++ src/main/java/com/kata/Calculator.java
@@ -8,19 +8,18 @@ public class Calculator {
      * @return int : returns an int which is the addition of utmost 2 int values
      */
     public static int Add(String numbers) {
-        int sum = 0;
+        float sum = 0;
         if (numbers != null && numbers.length() > 0) {
             String[] split = numbers.split(SEPARATOR);
-            //The method can take up to two numbers;hence i<2
-            for (int i = 0, j = 0; i < split.length && j < 2; i++) {
-                if (CalculatorUtil.isNumeric(split[i])) {
-                    sum += Float.parseFloat(split[i]);
-                    j++;
+            //The method can take any number of numbers and count them
+            for (String str:split) {
+                if (CalculatorUtil.isNumeric(str)) {
+                    sum += Float.parseFloat(str);
                 }
             }
 
         }
-        return sum;
+        return (int) sum;
     }
 
 
